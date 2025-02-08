1e816856c08ee8e1adcf09cbc7ebde5b428640df
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2025-02-08 13:12:16 +0200

    moved location

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-//console.log('Hello world')
-//const http = require('http')
-const express = require('express')
+const express = require('express') //otetaan käyttöön express (web-framework Node.js:lle, joka helpottaa palvelinpuolen kehitystä)
 const app = express()
-const cors = require('cors')
 
-
+//otetaan käyttöön express, joka on tällä kertaa funktio, jota kutsumalla luodaan muuttujaan app sijoitettava Express-sovellusta vastaava olio
 
 let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+const cors = require('cors')
+
+//    "build:ui": "rm -rf dist && cd C:/Users/laura/OneDrive - Laura-Kaisa Leinonen/Koulu/FullStackOpen/Harjoittelua/Osa2/harjoittelua2 && npm run build && cp -r dist C:/Users/laura/OneDrive - Laura-Kaisa Leinonen/Koulu/FullStackOpen/Harjoittelua/Osa3/Harjoittelua",
+//     "build:ui": "@powershell Remove-Item -Recurse -Force dist && cd \"C:/Users/laura/OneDrive - Laura-Kaisa Leinonen/Koulu/FullStackOpen/Harjoittelua/Osa2/harjoittelua2\" && npm run build && @powershell Copy-Item dist -Recurse \"C:/Users/laura/OneDrive - Laura-Kaisa Leinonen/Koulu/FullStackOpen/Harjoittelua/Osa3/Harjoittelua\"",
+//     "build:ui": "@powershell Remove-Item -Recurse -Force dist && cd C:/Users/laura/OneDrive - Laura-Kaisa Leinonen/Koulu/FullStackOpen/Harjoittelua/Osa2/harjoittelua2 && npm run build && @powershell Copy-Item dist -Recurse C:/Users/laura/OneDrive - Laura-Kaisa Leinonen/Koulu/FullStackOpen/Harjoittelua/Osa3/Harjoittelua",
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -32,15 +35,18 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(express.json())
 
+app.use(express.json())
 app.use(requestLogger)
+
 app.use(cors())
-app.use(express.static('dist'))
+//app.use(express.static('dist'))
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send( {error: 'unknown endpoint'} )
 }
+
+//Routet
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -49,23 +55,7 @@ app.get('/', (request, response) => {
 app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
-
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  if (note) {
-      response.json(note)
-  } else {
-      response.status(404).end()
-  }
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
+// Express muuntaa palautettavan datan automaattisesti JSON-muotoon. Ei siis ole JavaScript-olio vaan JSON-merkkijono.
 
 const generateId = () => {
   const maxID = notes.length > 0
@@ -77,7 +67,7 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (!body.content) { //content ei saa olla tyhjä
     return response.status(400).json({
       error: 'content missing'
     })
@@ -94,7 +84,23 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+  if (note) {
+      response.json(note)
+  } else {
+      response.status(404).end()
+  }
+})
 
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
 
 app.use(unknownEndpoint)
 
